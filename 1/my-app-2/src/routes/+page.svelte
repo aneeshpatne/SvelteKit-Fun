580a3f181c227912440700e7d8dfc07db4e6afaf
aneeshpatne <aneeshpatne@gmail.com>
2024-07-25 10:58:53 +0530

    clean up

--- 1/my-app-2/src/routes/+page.svelte
+++ 1/my-app-2/src/routes/+page.svelte
@@ -1,15 +1,25 @@
 <script>
     import {onMount} from 'svelte';
-    let Theme;
+    let Theme = true;
+    let Mounted = false;
+    function updateBodyClass(){
+        if (Mounted){
+        document.body.classList.remove(Theme ? 'light' : 'dark');
+        document.body.classList.add(Theme ? 'dark' : 'light');
+    }};
     function themeToggle(){
         Theme =! Theme;
-        localStorage.setItem('theme', Theme);
+        localStorage.setItem('theme', Theme ? 'dark' : ' light');
+        updateBodyClass();
     }
     onMount(()=>{
-        let var1 = localStorage.getItem('theme');
-        console.log(var1);
-        Theme = var1 ? var1 : true ; 
+        Mounted  = true;
+        const prevTheme = localStorage.getItem('theme');
+        Theme = prevTheme ==='dark'; 
+        updateBodyClass();
         });
+
+    
 </script>
 {Theme}
 <button on:click={() => themeToggle()}>Click Me</button>
